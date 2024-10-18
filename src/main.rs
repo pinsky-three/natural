63e1b1fff7d7739e17849bea0d9bb472e1deae60
Bregy Malpartida <bregy@minsky.cc>
2024-10-18 17:51:03 -0500

    feat: Add initial project files and configuration

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
+const X_EXTENT: f32 = 600.;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
+        .add_systems(Startup, setup)
+        // .add_systems(Update, blink_system)
+        // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
+        // or after the `EguiSet::BeginPass` system (which belongs to the `CoreSet::PreUpdate` set).
+        .add_systems(Update, ui_example_system)
+        .run();
+}
+
+fn ui_example_system(mut contexts: EguiContexts) {
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    });
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    let shapes = [
+        Mesh2dHandle(meshes.add(Circle { radius: 35.0 })),
+        Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
+        Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
+        Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
+        Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
+        Mesh2dHandle(meshes.add(Triangle2d::new(
+            Vec2::Y * 50.0,
+            Vec2::new(-50.0, -50.0),
+            Vec2::new(50.0, -50.0),
+        ))),
+    ];
+
+    let num_shapes = shapes.len();
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        // Distribute colors evenly across the rainbow.
+        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: shape,
+            material: materials.add(color),
+            transform: Transform::from_xyz(
+                // Distribute shapes from -X_EXTENT to +X_EXTENT.
+                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+                0.0,
+                0.0,
+            ),
+            ..default()
+        });
+    }
+}
+
+// fn blink_system(time: Res<Time>, mut query: Query<(&mut Transform, &Handle<Mesh>)>) {
+//     println!("blink");
+//     for (mut transform, _mesh) in query.iter_mut() {
+//         // Use sinusoidal function to oscillate scale over time
+//         let scale = 1.0 + (time.elapsed_seconds().sin() * 0.1);
+//         transform.scale = Vec3::splat(scale);
+
+//         println!("transform: {:?}", transform);
+//     }
+// }
